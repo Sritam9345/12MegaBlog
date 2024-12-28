23c9e03131e3d0e941104451984967dbd836c4b0
Sritam9345 <b522057@iiit-bh.ac.in>
2024-12-28 19:52:36 +0530

    first commit

---
+++ src/appwrite/config.js
@@ -0,0 +1,147 @@
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+    
+    constructor(){
+        this.client
+        .setEndpoint("https://cloud.appwrite.io/v1")
+        .setProject("675d285b0007ce619a7f");
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+
+            console.log(typeof featuredImage);
+
+            return await this.databases.createDocument(
+                "675d29a2002993b7d4e4",
+                "675d29d40038709c9128",
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                "675d29a2002993b7d4e4",
+                "675d29d40038709c9128",
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                "675d29a2002993b7d4e4",
+                "675d29d40038709c9128",
+                slug
+            
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                "675d29a2002993b7d4e4",
+                "675d29d40038709c9128",
+                slug
+            
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                "675d29a2002993b7d4e4",
+                "675d29d40038709c9128",
+                queries,
+                
+
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                "675d2c53003d2b68cfa5",
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                "675d2c53003d2b68cfa5",
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+       
+       if(!fileId){
+        return console.log("Error");
+        
+       }
+       
+       
+        return this.bucket.getFilePreview(
+            "675d2c53003d2b68cfa5",
+            fileId
+        )
+    }
+}
+
+
+const service = new Service()
+export default service
